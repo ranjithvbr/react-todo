ac3b3a9216ad8e74a4211ece08f5ab26269c1a81
rathish <rathishvbr@megam.io>
2019-01-25 15:35:39 +0530

    Simple UI updated, TODO: Document, test case

--- src/components/TodoLanding.js
+++ src/components/TodoLanding.js
@@ -3,92 +3,96 @@ import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 
 export default class TodoList extends React.Component {
-  state = {
-    todos: []
-  };
+    state = {
+        todos: []
+    };
 
-  addTodo = todo => {
-    this.setState(state => ({
-      todos: [todo, ...state.todos]
-    }));
-  };
+    addTodo = todo => {
+        this.setState(state => ({
+            todos: [
+                todo, ...state.todos
+            ]
+        }));
+    };
 
-  toggleComplete = id => {
-    this.setState(state => ({
-      todos: state.todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            complete: !todo.complete
-          };
-        } else {
-          return todo;
-        }
-      })
-    }));
-  };
+    toggleComplete = id => {
+        this.setState(state => ({
+            todos: state
+                .todos
+                .map(todo => {
+                    if (todo.id === id) {
+                        return {
+                            ...todo,
+                            complete: !todo.complete
+                        };
+                    } else {
+                        return todo;
+                    }
+                })
+        }));
+    };
 
-  handleDeleteTodo = id => {
-    this.setState(state => ({
-      todos: state.todos.filter(todo => todo.id !== id)
-    }));
-  };
+    handleDeleteTodo = id => {
+        this.setState(state => ({
+            todos: state
+                .todos
+                .filter(todo => todo.id !== id)
+        }));
+    };
 
-  handleUpdateTodo = (id, updateText) => {
-    this.setState(state => {
-      todos: state.todos.map(todo => {
-        if(todo.id === id) {
-          todo.text = updateText;
-        }
-        return todo;
-      })})
-  };
+    handleUpdateTodo = (id, updateText) => {
+        this.setState(state => {
+          // eslint-disable-next-line
+            todos : state 
+                .todos
+                .map(todo => {
+                    if (todo.id === id) {
+                        todo.text = updateText;
+                    }
+                    return todo;
+                })
+        })
+    };
 
-  render() {
-    let todosComplete = [];
-    let todosNotComplete = [];
+    render() {
+        let todosComplete = [];
+        let todosNotComplete = [];
 
-    this.state.todos.forEach(todo => {
-      if(todo.complete) {
-        todosComplete = [todo, ...todosComplete];
-      } else {
-        todosNotComplete = [todo, ...todosNotComplete];
-      }
-    })
+        this
+            .state
+            .todos
+            .forEach(todo => {
+                if (todo.complete) {
+                    todosComplete = [
+                        todo, ...todosComplete
+                    ];
+                } else {
+                    todosNotComplete = [
+                        todo, ...todosNotComplete
+                    ];
+                }
+            })
 
-    return (
-      <div className="container">
-        <h2>Add Item</h2>
-        <TodoForm onSubmit={this.addTodo} />
-        <hr/>
-        <h2>Todo</h2>
-        <div>
-          Left: {todosNotComplete.length}
-        </div>
-        {todosNotComplete.map(todo => (
-          <Todo
-            key={todo.id}
-            toggleComplete={() => this.toggleComplete(todo.id)}
-            onDelete={() => this.handleDeleteTodo(todo.id)}
-            onUpdate={this.handleUpdateTodo}
-            todo={todo}
-          />
-        ))}
-                <hr/>
-        <h2>Completed</h2>
-        <div>
-          okay: {todosComplete.length}
-        </div>
-        {todosComplete.map(todo => (
-          <Todo
-            key={todo.id}
-            toggleComplete={() => this.toggleComplete(todo.id)}
-            onDelete={() => this.handleDeleteTodo(todo.id)}
-            onUpdate={this.handleUpdateTodo}
-            todo={todo}
-          />
-        ))}
-      </div>
-    );
-  }
+        return (
+            <div className="container">
+                <h3>Add Item</h3>
+                <TodoForm onSubmit={this.addTodo}/>
+                <h3>Todo {todosNotComplete.length}</h3>
+                {todosNotComplete.map(todo => (
+                <Todo
+                    key={todo.id}
+                    toggleComplete={() => this.toggleComplete(todo.id)}
+                    onDelete={() => this.handleDeleteTodo(todo.id)}
+                    onUpdate={this.handleUpdateTodo}
+                    todo={todo}/>))}
+                <h3>Completed  {todosComplete.length}</h3> 
+                {todosComplete.map(todo => (<Todo
+                    key={todo.id}
+                    toggleComplete={() => this.toggleComplete(todo.id)}
+                    onDelete={() => this.handleDeleteTodo(todo.id)}
+                    onUpdate={this.handleUpdateTodo}
+                    todo={todo}/>))}
+            </div>
+        );
+    }
 }
--- src/components/TodoText.js
+++ src/components/TodoText.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+//Just stateless component show the text with strike or show normally based on strike value
 export default props => {
   return props.strike ? <strike>{props.todoText}</strike> : <span>{props.todoText}</span>;
 };
+ 
