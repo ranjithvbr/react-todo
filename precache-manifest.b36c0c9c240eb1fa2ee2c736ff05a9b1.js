0ca536c92bc67f4cc31e1b9776f3c357ca04bc28
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 13:22:38 +0000

    Deploy rathishvbr/react-todo to github.com/rathishvbr/react-todo.git:gh-pages

---
+++ precache-manifest.b36c0c9c240eb1fa2ee2c736ff05a9b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1e9e9856fe345addd08",
+    "url": "/react-todo/static/css/main.fe536017.chunk.css"
+  },
+  {
+    "revision": "b1e9e9856fe345addd08",
+    "url": "/react-todo/static/js/main.b1e9e985.chunk.js"
+  },
+  {
+    "revision": "4b173e77fae30f03c42d",
+    "url": "/react-todo/static/css/1.e79deedc.chunk.css"
+  },
+  {
+    "revision": "4b173e77fae30f03c42d",
+    "url": "/react-todo/static/js/1.4b173e77.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "e0068bdfd772c148cb0ddf3dfcf58acb",
+    "url": "/react-todo/index.html"
+  }
+];
